7c1f19a786c8a9b6c728e42510207696c7fe03d4
Ángel González <angotov@gmail.com>
2020-01-21 17:21:22 +0100

    Revert "Merge branch 'master' of https://github.com/paupedra/Gran_CITMGameJam_VibeCheck"
    This reverts commit a8e063d54fc11f44e82cd02ff07ac53b52057f17, reversing
    changes made to 67cf002433a37ff97ec5d0529112038265e19a36.

---
+++ True Vibe Check/Assets/Level_Changer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Changer : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoadIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 1)
+        {
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            FadeToLevel(0);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {   
+        levelToLoadIndex = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoadIndex);
+    }
+
+
+
+}
