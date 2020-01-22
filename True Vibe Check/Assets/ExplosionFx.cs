0ad7cd917542bbfeceb69387e808332be3db2dd0
Aitorlb7 <45202115+Aitorlb7@users.noreply.github.com>
2020-01-22 13:55:25 +0100

    Explosion audio again

--- True Vibe Check/Assets/ExplosionFx.cs
+++ True Vibe Check/Assets/ExplosionFx.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class ExplosionFx : MonoBehaviour
 {
 
-    //public GameObject PlayerObj;
-    //private Player player;
+    public GameObject PlayerObj;
+    private PlayerMovement player;
+    public AudioSource explosion;
+    bool played;
     // Start is called before the first frame update
     void Start()
     {
-        //player = PlayerObj.GetComponent<Player>();
+        player = PlayerObj.GetComponent<PlayerMovement>();
+        explosion = explosion.GetComponent<AudioSource>();
+        played = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+     if(player.dead && !played)
+        {
+            explosion.Play();
+            played = true;
+        }
     }
 }
