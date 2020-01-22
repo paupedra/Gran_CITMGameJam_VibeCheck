85dd07a53ccbd2e2d5e147797bce8215940b58d5
Ángel González <angotov@gmail.com>
2020-01-22 10:42:18 +0100

    First scene door and scene transition implementation

---
+++ True Vibe Check/Assets/Scripts/Scene_Transitioner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Transitioner : MonoBehaviour
+{
+    public Level_Changer changer;
+    public CapsuleCollider2D playerCollider;
+    public BoxCollider2D transitionerCollider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerCollider.IsTouching(transitionerCollider))
+        {
+            changer.FadeToNextLevel();
+        }
+    }
+}
